1d5acfdc44bddaba0f7af0685d00f909523d7c4a
Yasir_Hassan <yasir.hassan7722@gmail.com>
2023-10-27 05:15:27 -0500

    Add project files.

---
+++ ASPdotNetCoreMiddleware/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// use Use() method extension to excute a non terminating middleware
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello, and ");
+    await next(context);
+});
+
+// use Use() extension method to excute a non terminating middleware
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello again. ");
+    await next(context);
+});
+
+// use Run() extension method to execute a terminating middleware
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("Hello3 from a teminating mdw");
+    
+});
+
+app.Run();
