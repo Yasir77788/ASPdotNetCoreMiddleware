6274a654c914e6119174c58d18c5840fec74f023
Yasir_Hassan <yasir.hassan7722@gmail.com>
2023-10-27 09:19:40 -0500

    add a custom middleware class to seperate the logic for lambda expersion

--- ASPdotNetCoreMiddleware/Program.cs
+++ ASPdotNetCoreMiddleware/Program.cs
@@ -1,19 +1,26 @@
+using ASPdotNetCoreMiddleware.CustomMiddleware;
+
 var builder = WebApplication.CreateBuilder(args);
+// register the Custom Middleware class as a service
+builder.Services.AddTransient<MyCustomMiddleware>();  
 var app = builder.Build();
 
 // use Use() extension method to excute a non terminating middleware
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello, and ");
+    await context.Response.WriteAsync("Hello1, and ");
     await next(context);
 });
 
 // use Use() extension method to excute a non terminating middleware
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello again. ");
+    await context.Response.WriteAsync("Hello2 again. ");
     await next(context);
 });
+
+// register the customer middleware class - MyCustomMiddleware
+app.UseMiddleware<MyCustomMiddleware>();    
 
 // use Run() extension method to execute a terminating middleware
 app.Run(async (HttpContext context) =>
